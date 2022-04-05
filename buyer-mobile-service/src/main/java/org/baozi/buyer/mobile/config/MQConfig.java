554138056ce036ac8d7855bb4def1a45cafc73e5
baozi <bigbaozibao@qq.com>
2022-04-05 17:00:23 +0800

    手动ack

--- buyer-mobile-service/src/main/java/org/baozi/buyer/mobile/config/MQConfig.java
+++ buyer-mobile-service/src/main/java/org/baozi/buyer/mobile/config/MQConfig.java
@@ -1,19 +1,30 @@
 package org.baozi.buyer.mobile.config;
 
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
 
+import java.io.IOException;
 import java.util.function.Consumer;
 
 @Configuration
 //@EnableBinding(Sink.class)
 public class MQConfig {
     @Bean
-    public Consumer<String> testOutput() {
-        return System.out::println;
+    public Consumer<Message<String>> testOutput() {
+        return message -> {
+            System.out.println(message.getPayload());
+            Channel channel = message.getHeaders().get(AmqpHeaders.CHANNEL, Channel.class);
+            Long tag = message.getHeaders().get(AmqpHeaders.DELIVERY_TAG, Long.class);
+            try {
+                if (channel != null && tag != null)
+                    channel.basicAck(tag, false);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
     }
 
 //    @StreamListener(target = Sink.INPUT)
